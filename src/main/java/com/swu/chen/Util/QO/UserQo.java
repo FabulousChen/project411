1c1e168aef2ae8b972ab841935e6fa42d08cdde1
DUDUJL <38250914+DUDUJL@users.noreply.github.com>
2018-09-08 14:23:06 +0800

    增加查询对象

---
+++ src/main/java/com/swu/chen/Util/QO/UserQo.java
@@ -0,0 +1,15 @@
+package com.swu.chen.Util.QO;
+
+import lombok.Data;
+
+@Data
+public class UserQo {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+}
